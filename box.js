2478557ba73ef2568f93113139c8993936fac3a9
Aryan <76473160+AryanPardeshi@users.noreply.github.com>
2021-03-17 11:12:34 -0400

    Add files via upload

--- box.js
+++ box.js
@@ -2,7 +2,10 @@ class Box {
     constructor(x,y,width,height) {
 
         var options= {
-            restitution:0.8
+            'restitution':6,
+            'friction':0.3,
+            'density':1.0
+
         }
         this.body=Bodies.rectangle(x,y,width,height,options);
         this.x=x;
@@ -16,6 +19,5 @@ display() {
     rectMode(CENTER)
     fill(rgb(255,0,0))
     rect(this.y,this.x,this.width,this.height);
-    
 }
 }
